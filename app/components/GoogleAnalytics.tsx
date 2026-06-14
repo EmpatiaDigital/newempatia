72f0ea8adeb1a6e2b7ecada0066bc99be16885e0
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-06-14 01:25:46 -0300

    Add files via upload

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const GA_MEASUREMENT_ID = 'G-1PQVGSKJGE';
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window.gtag !== 'function') return;
+
+    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
+
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
