eb4ca9027f02791ee7dcba3437b8a7c6248e464c
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-06-15 17:11:35 -0300

    Update page.tsx

--- app/Statsbanner/page.tsx
+++ app/Statsbanner/page.tsx
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 import "../style/StatsBanner.css";
 
 // ── Tipos ─────────────────────────────────────────────────────────────────────
+
 interface BannerConfig {
   _id?: string;
   textoBase: string;
